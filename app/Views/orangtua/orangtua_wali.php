b84197dd9a425e27c08040f8974f869b797eccef
AndreasTopuh <andreasjeno23@gmail.com>
2025-03-29 15:26:15 +0800

    fixing

--- app/Views/orangtua/orangtua_wali.php
+++ app/Views/orangtua/orangtua_wali.php
@@ -30,7 +30,7 @@
             top: 0;
             left: 0;
             width: 100%;
-            height: 120%;
+            height: 100%;
             background: inherit;
             filter: blur(10px); 
             z-index: -1; 
