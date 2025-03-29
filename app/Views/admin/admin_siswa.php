2103f60994462b57448fe76730f4c3704ff6c316
chaddjong <richardicad85@gmail.com>
2025-03-29 12:20:07 +0800

    testt

--- app/Views/admin/admin_siswa.php
+++ app/Views/admin/admin_siswa.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin3</title>
+    <title>Admin Siswa</title>
     <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
     <style>
         /* Import Google Font */
